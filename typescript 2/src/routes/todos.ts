2fed2a9193111a2a36ef0c93b4aaedbb973760f0
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-30 12:37:35 +0530

    used type casting for req.body and req.params

--- typescript 2/src/routes/todos.ts
+++ typescript 2/src/routes/todos.ts
@@ -5,30 +5,39 @@ import { todo } from '../models/todo';
 
 let todos: todo [] = [] 
 
+type RequestBody = { text:string }
+type RequestParams = { id : string}
+
 router.get('/' , (req,res,next)=>{
     res.status(200).json({ todos : todos })
 })
 
 router.post('/todo', (req,res,next)=>{
+
+    const Body = req.body as RequestBody
+
     const newToDo: todo = {
         id:new Date().toISOString(),
-        text: req.body.text 
+        text: Body.text 
     }
-
     todos.push(newToDo);
     res.status(201).json({message: ' added todos' , todo:newToDo})
 })
 
 router.delete('/delete-todo/:id' , (req,res,next)=>{
-    todos = todos.filter(todo => todo.id !== req.params.id)
+    const Params = req.params as RequestParams
+    todos = todos.filter(todo => todo.id !== Params.id)
     res.status(200).json({message:'deleted' , todos:todos})
 })
 
 router.put('/eddit-todo/:id' , (req,res,next)=>{
-    const id = req.params.id ;
+    const Body = req.body as RequestBody
+    const Params = req.params as RequestParams
+
+    const id = Params.id ;
     const index = todos.findIndex(todoItem => todoItem.id === id);
     if(index >= 0 ){
-        todos[index] = { id: todos[index].id , text:req.body.text};
+        todos[index] = { id: todos[index].id , text:Body.text};
         return res.status(200).json({message: 'updated todo' , todos : todos})
     }
     res.status(404).json({message:'could not find todo'})
